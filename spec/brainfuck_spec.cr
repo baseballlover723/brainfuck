12b3c060455aea0b3f3a8977297d89e120cff8f5
Philip Ross <dev@baseballlover723.com>
2022-02-05 18:52:28 -0800

    now accepts input and output io streams

--- spec/brainfuck_spec.cr
+++ spec/brainfuck_spec.cr
@@ -4,14 +4,36 @@ describe Brainfuck do
   describe "#new " do
     {% for cell_size in Brainfuck::CELL_SIZES %}
       it "accepts cell size of #{{{cell_size}}}" do
-        Brainfuck({{cell_size}}).new
+        Brainfuck({{cell_size}}).new(input: IO::Memory.new, output:IO::Memory.new)
       end
     {% end %}
 
     it "rejects invalid cell sizes" do
       expect_raises(ArgumentError) do
-        Brainfuck(String).new
+        Brainfuck(String).new(input: IO::Memory.new, output: IO::Memory.new)
       end
+    end
+
+    it "defaults input to STDIN" do
+      bf = Brainfuck(Int8).new(output: IO::Memory.new)
+      bf.input.should be(STDIN)
+    end
+
+    it "accepts an input io" do
+      io = IO::Memory.new
+      bf = Brainfuck(Int8).new(input: io, output: IO::Memory.new)
+      bf.input.should be(io)
+    end
+
+    it "defaults output to STDOUT" do
+      bf = Brainfuck(Int8).new(input: IO::Memory.new)
+      bf.output.should be(STDOUT)
+    end
+
+    it "accepts an output io" do
+      io = IO::Memory.new
+      bf = Brainfuck(Int8).new(input: IO::Memory.new, output: io)
+      bf.output.should be(io)
     end
   end
 end
--- src/brainfuck.cr
+++ src/brainfuck.cr
@@ -1,9 +1,12 @@
 class Brainfuck(T)
   VERSION = "0.1.0"
 
+  property output : IO
+  property input : IO
+
   CELL_SIZES = {Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128}
 
-  def initialize
+  def initialize(*, @input : IO = STDIN, @output : IO = STDOUT)
     raise ArgumentError.new("invalid cell size") unless CELL_SIZES.includes?(T)
   end
 end
