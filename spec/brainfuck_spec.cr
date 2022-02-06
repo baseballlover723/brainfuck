fbc5c006540410d2538af0f0fa3bc191d796ef26
Philip Ross <dev@baseballlover723.com>
2022-02-05 20:13:50 -0800

    added some super basic tests

--- spec/brainfuck_spec.cr
+++ spec/brainfuck_spec.cr
@@ -1,13 +1,7 @@
 require "./spec_helper"
 
 describe Brainfuck do
-  describe "#new " do
-    {% for cell_size in Brainfuck::CELL_SIZES %}
-      it "accepts cell size of #{{{cell_size}}}" do
-        Brainfuck({{cell_size}}).new(input: IO::Memory.new, output:IO::Memory.new)
-      end
-    {% end %}
-
+  describe "#new" do
     it "rejects invalid cell sizes" do
       expect_raises(ArgumentError) do
         Brainfuck(String).new(input: IO::Memory.new, output: IO::Memory.new)
@@ -36,4 +30,72 @@ describe Brainfuck do
       bf.output.should be(io)
     end
   end
+
+  describe "#run" do
+    it "can run with a string" do
+      bf = Brainfuck(Int8).new(input: IO::Memory.new, output: IO::Memory.new)
+      bf.run(File.read("#{__DIR__}/test_files/hello_world.bf"))
+    end
+
+    it "can run with a path" do
+      bf = Brainfuck(Int8).new(input: IO::Memory.new, output: IO::Memory.new)
+      bf.run(Path.new("#{__DIR__}/test_files/hello_world.bf"))
+    end
+
+    it "can run with an io" do
+      bf = Brainfuck(Int8).new(input: IO::Memory.new, output: IO::Memory.new)
+      bf.run(IO::Memory.new(File.read("#{__DIR__}/test_files/hello_world.bf")))
+    end
+
+    it "can run with a file" do
+      bf = Brainfuck(Int8).new(input: IO::Memory.new, output: IO::Memory.new)
+      File.open("#{__DIR__}/test_files/hello_world.bf", "r") do |file|
+        bf.run(file)
+      end
+    end
+  end
 end
+
+{% for cell_size in Brainfuck::CELL_SIZES %}
+describe Brainfuck({{cell_size}}) do
+  it "accepts a cell size of #{{{cell_size}}}" do
+    Brainfuck({{cell_size}}).new(input: IO::Memory.new, output: IO::Memory.new)
+  end
+
+  describe "it successfully runs" do
+    Dir["#{__DIR__}/test_files/*.bf"].each do |instruction_file|
+      output_file = instruction_file[0...-File.extname(instruction_file).size] + ".output"
+      next unless File.exists?(output_file)
+      test_name = File.basename(instruction_file)
+      expected_output = File.read(output_file)
+
+      it test_name do
+        input = IO::Memory.new
+        output = String.build do |output_io|
+          bf = Brainfuck(Int8).new(input: input, output: output_io)
+          bf.run(instruction_file)
+        end
+
+        output.should eq(expected_output)
+      end
+    end
+  end
+
+  describe "it successfully errors on" do
+    Dir["#{__DIR__}/test_files/*.bf"].each do |instruction_file|
+      output_file = instruction_file[0...-File.extname(instruction_file).size] + ".error"
+      next unless File.exists?(output_file)
+      test_name = File.basename(instruction_file)
+
+      it test_name do
+        input = IO::Memory.new
+        output = IO::Memory.new
+        bf = Brainfuck(Int8).new(input: input, output: output)
+        expect_raises(Exception) do
+          bf.run(instruction_file)
+        end
+      end
+    end
+  end
+end
+{% end %}
